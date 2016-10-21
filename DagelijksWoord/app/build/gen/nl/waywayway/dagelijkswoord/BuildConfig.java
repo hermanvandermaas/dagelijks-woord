8fe24858cdd2cc9d5e5b5bf72ac981fc9b853e9e
Herman van der Maas <herman@waywayway.nl>
2016-10-21 16:14:14 +0200

    Volgende

---
+++ DagelijksWoord/app/build/gen/nl/waywayway/dagelijkswoord/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package nl.waywayway.dagelijkswoord;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
---
+++ DagelijksWoord/app/src/main/java/nl/waywayway/dagelijkswoord/MainActivity.java
@@ -0,0 +1,15 @@
+package nl.waywayway.dagelijkswoord;
+
+import android.app.*;
+import android.os.*;
+import android.support.v7.app.*;
+
+public class MainActivity extends AppCompatActivity
+{
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
